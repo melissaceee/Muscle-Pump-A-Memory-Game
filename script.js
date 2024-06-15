1ffec3b747956e672dc75036126c18ced8a36d53
melissaceee <cheng.melissa@outlook.com>
2024-06-15 12:17:32 -0400

    update to code for clearer flow

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ let cards = [
 ];
 
 const gameBoard = document.getElementById('game-board');
+
 let clickedCards = [];
 let matchedCards = [];
 let interval
 
-let startingTime = 45;
+let startingTime = 60;
 const timer = document.getElementById('time-left');
+
 
 function updateTimer() {
     timer.innerHTML = `${startingTime}`;
@@ -39,7 +41,7 @@ function startGame() {
     cardValue.forEach((div, index) => {
         div.dataset.value = cards[index];
         div.addEventListener('click', showCard);
-    })
+    });
 
     showAllCards();
     setTimeout(hideAllCards, 5000)
@@ -61,14 +63,13 @@ function hideAllCards() {
     interval = setInterval(updateTimer, 1000);
 }
 
-startGame();
 
 function showCard() {
     const isClicked = this.classList.contains('clicked');
     const isMatched = this.classList.contains('matched');
-    const canFlip = clickedCards.length < 2 && !isClicked && !isMatched;
+    const notClickedorMatched = !isClicked && !isMatched;
 
-    if (canFlip) {
+    if (clickedCards.length <2 && notClickedorMatched) {
         this.textContent = this.dataset.value;
         this.classList.add('clicked');
         clickedCards.push(this);
@@ -79,27 +80,31 @@ function showCard() {
     }
 }
 
+
+
 function checkMatch() {
     const [card1, card2] = clickedCards;
     if (card1.dataset.value === card2.dataset.value) {
         card1.classList.add('matched');
         card2.classList.add('matched');
-        matchedCards.push(card1, card2);
     } else {
-        setTimeout(() => {
             card1.classList.remove('clicked');
             card2.classList.remove('clicked');
             card1.textContent = '';
             card2.textContent = '';
-        }, 500);
-    }
+        }
     clickedCards = [];
+    checkWin()
 
+    }
+  function checkWin() {
     if (matchedCards.length === cards.length) {
         clearInterval(interval);
         const results = document.getElementById('game-result');
         results.classList.add('game-result-win');
         setTimeout(() => results.innerHTML = 'Winner winner chicken dinner!!!', 500);
     }
-}
+  }
+    
 
+startGame();
