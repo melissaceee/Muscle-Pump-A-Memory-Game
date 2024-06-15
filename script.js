26c4126aa7a2811785c27a1c6f1f4e95f935b6aa
melissaceee <cheng.melissa@outlook.com>
2024-06-14 21:29:23 -0400

    updates

--- script.js
+++ script.js
@@ -1,49 +1,35 @@
-    const cards = [
-        '1','2','3','4','5','6','7','8','9','10',
-        '1','2','3','4','5','6','7','8','9','10'
-    ];
+let cards = [
+    '1','2','3','4','5','6',
+    '1','2','3','4','5','6'
+];
 
-    const gameBoard = document.getElementById ('game-board')
+const gameBoard = document.getElementById('game-board');
+let clickedCards = [];
+let matchedCards = [];
+let interval
 
-    let flippedCards = [];
-    let matchedCards = [];
-
-
-    let startingTime = 5 
-    const timer = document.getElementById('time-left');
-    function updateTimer (){
-        timer.innerHTML = `${startingTime}`
-        startingTime-=1 
-        if(startingTime < 0) {
-            clearInterval (interval);
-            const results = document.getElementById('game-result');  
-            results.innerHTML = `${'You Lost!!!'}`
-        }
-    }
-    const interval = setInterval(updateTimer, 1000) 
-    
-    function shuffle(array){
-        for (let i = 0; i<array.length; i++){
-           let randomNumber =  Math.floor (Math.random()* array.length)
-           let temp = array [i]
-           array[i] = array[randomNumber]
-           array [randomNumber] = temp
-        }
+let startingTime = 30;
+const timer = document.getElementById('time-left');
+function updateTimer() {
+    timer.innerHTML = `${startingTime}`;
+    startingTime -= 1;
+    if (startingTime < 0) {
+        clearInterval(interval);
+        const results = document.getElementById('game-result');
+        results.classList.add('game-result-lost');
+        setTimeout(() => results.innerHTML = 'You Lost!!!', 500);
 
     }
+}
 
-    function resetGame(){
-        shuffle[cards]
-        cards.forEach (cardValue=>{
-            const div = document.createElement('div');
-            div.classList.add('card');
-            div.dataset.value =cardValue;
-            
-        })
+function shuffle(array){
+    for (let i = 0; i<array.length; i++){
+       let randomNumber =  Math.floor (Math.random()* array.length)
+       let temp = array [i]
+       array[i] = array[randomNumber]
+       array [randomNumber] = temp
     }
 
+}
 
-
- 
-
-
+function startGame() {
